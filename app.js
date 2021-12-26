352118e5245e523799c20583a848474f7d07ea72
Bumsuk Ha <bumsuk0811@gmail.com>
2021-12-26 20:48:32 +0900

    third commit

--- app.js
+++ app.js
@@ -1,58 +1,65 @@
-const h1 = document.querySelector("div#something h1:nth-child(1)")
+const h1 = document.querySelector("#something h1:nth-child(1)");
+const body = document.body;
 
-function reset(){
-    h1.style.color = "white";
-    h1.style.fontSize = "32px"
-    h1.innerText = "Merry Christmas!"
+const white = "white";
+const blue = "blue";
+const gray = "gray";
+const pink = "pink";
+const tomato = "tomato";
+const brightGray = "brightGray";
+const backBlueGray = "backBlueGray";
+const backBeige = "backBeige";
+
+h1.className = white;
+body.className = backBlueGray;
+
+function reset() {
+  if (body.className == backBlueGray) {
+    h1.className = white;
+    h1.style.fontSize = "32px";
+    h1.innerText = "Merry Christmas!";
+  } else if (body.className == backBeige) {
+    h1.className = gray;
+    h1.style.fontSize = "32px";
+    h1.innerText = "Merry Christmas!";
+  }
 }
 
-function resetWindow(){
-    h1.style.color = "#1f1f1f";
-    h1.style.fontSize = "32px"
-    h1.innerText = "Merry Christmas!"
+function handleTitleClick() {
+  let h1Text;
+  const currentColor = h1.className;
+  let newColor;
+  h1Text = "Mouse was cilked!";
+  h1.innerText = h1Text;
+
+  if (currentColor == tomato) {
+    newColor = pink;
+  } else {
+    newColor = tomato;
+  }
+  h1.className = newColor;
 }
 
-function handleTitleClick(){
-    let h1Text;
-    const currentColor = h1.style.color;
-    let newColor;
-    h1Text = "Mouse was cilked!"
-    h1.innerText = h1Text
-
-    if(currentColor == "tomato"){
-        newColor = "#fd5c63"
-    }
-    else{
-        newColor = "tomato"
-    }
-    h1.style.color = newColor;
-}
-
-function handleTitleEnter(){
-    h1.innerText = "Mouse is here!"
-    h1.style.fontSize = "35px"
-    h1.style.color = "#666666"
+function handleTitleEnter() {
+  h1.innerText = "Mouse is here!";
+  h1.style.fontSize = "35px";
+  h1.className = brightGray;
 }
 
 function handleTitleMouseLeave() {
-    h1.innerText = "Mouse is gone!"
-    h1.style.fontSize = "32px"
-    h1.style.color = "#6699CC"
-    if(document.body.style.backgroundColor == "beige"){
-        setTimeout(resetWindow, 500);
-    }
-    else{
-        setTimeout(reset, 500);
-    }
+  h1.innerText = "Mouse is gone!";
+  h1.style.fontSize = "32px";
+  h1.className = blue;
+  setTimeout(reset, 500);
 }
 
 function handleWindeResize() {
-    document.body.style.backgroundColor = "beige"
-    resetWindow()
+  body.className = backBeige;
+  reset();
 }
 
-h1.addEventListener("click", handleTitleClick)
-h1.addEventListener("mouseenter", handleTitleEnter)
-h1.addEventListener("mouseleave", handleTitleMouseLeave)
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleTitleEnter);
+h1.addEventListener("mouseleave", handleTitleMouseLeave);
 
-window.addEventListener("resize", handleWindeResize)
+window.addEventListener("resize", handleWindeResize);
